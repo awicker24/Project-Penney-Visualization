0aef9202d61580914af9c58d0f21aece042aa796
awicker24 <acwicker@wm.edu>
2024-09-27 13:12:27 -0400

    Update heatmap.py

--- draft_heatmap/heatmap.py
+++ draft_heatmap/heatmap.py
@@ -1,7 +1,6 @@
 ## Heatmap code to work on
 ### starting with group 1 (since it takes in an array)
 
-
 import seaborn as sns
 import matplotlib.pyplot as plt
 from matplotlib.colors import LinearSegmentedColormap
@@ -10,41 +9,27 @@ import os
 import json
 import matplotlib.ticker as mtick
 
-num_cards_file = 'data/game_data_total_cards.json'
-num_tricks_file = 'data/game_data_tricks.json'
+#initialize
+card_wins_matrix = np.zeros((8,8))
+trick_wins_matrix = np.zeros((8,8))
+#print(card_wins_matrix)
+#print(trick_wins_matrix)
 
-def matrix_creator(win_counts_file):
-    combinations = ['BBB', 'BBR', 'BRB', 'BRR', 'RBB', 'RBR', 'RRB', 'RRR']
-    matrix = pd.DataFrame(0.0, index=combinations, columns=combinations)
+#fill
+i = 0
+for x in range(8):
+    for z in range(8):
+        card_wins_matrix[x,z] = i
+        trick_wins_matrix[x,z] = 64-i
+        i+=1
 
-    # load the json data
-    if os.path.exists(win_counts_file):                # Check if the win counts file exists
-        with open(win_counts_file, 'r') as f:          # Open the file in read mode
-            win_counts = json.load(f)                  # Load the win counts
-            #return win_counts['player1_wins'], win_counts['player2_wins'] # Return the win counts
-    else:
-        print('File not found!')
-    
-    num_rounds = win_counts['total_rounds_played']
+#print(card_wins_matrix)
+#print()
+#print(trick_wins_matrix)
 
-    games = win_counts['win_data']
-    for game in games:
-        # strip the player combos to index the matrix at correct loc
-        p1 = game[0:3]
-        p2 = game[7:10]
-        # calculate the probabilites for the win
-        num_wins = win_counts['win_data'][game]
-        prob = num_wins / num_rounds
-        
-        # save the data in the matrix
-        matrix.loc[p1, p2] = float(prob)
-
-        #print(win_counts['win_data'][game])
-
-    return matrix
-
-num_cards_result = matrix_creator(num_cards_file)
-num_tricks_result = matrix_creator(num_tricks_file)
+#save
+np.save("card_wins.npy",card_wins_matrix)
+np.save("trick_wins.npy",trick_wins_matrix)
 
 sequences = ['BBB', 'BBR', 'BRB', 'BRR', 'RBB', 'RBR', 'RRB', 'RRR']
 def create_heatmaps(num_cards = num_cards_result, num_tricks = num_tricks_result):
